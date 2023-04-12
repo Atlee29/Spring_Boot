12dfb8fba4f789df6d7de19afee817202c262c6c
Atlee29 <pkbarada@outlook.com>
2023-04-12 18:14:35 +0530

    Spring webapp created

---
+++ springBoot-1/src/main/java/com/boot/main/SpringBoot1Application.java
@@ -0,0 +1,14 @@
+package com.boot.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello I Am Actuator ....Me springBoot me aata hu samaj me nahi...");
+		SpringApplication.run(SpringBoot1Application.class, args);
+	}
+
+}
